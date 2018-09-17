62c81e515083beca2e4a278359fb551c7d171902
MoeRookie <MoeRookie@163.com>
2018-09-17 13:57:44 +0800

    打印设备中可启动的应用个数

--- app/src/main/java/com/ghsoft/nearlauncher/NearLauncherFragment.java
+++ app/src/main/java/com/ghsoft/nearlauncher/NearLauncherFragment.java
@@ -1,15 +1,22 @@
 package com.ghsoft.nearlauncher;
 
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 public class NearLauncherFragment extends Fragment{
+    private static final String TAG = "NearLauncherFragment";
 
     private RecyclerView mRecyclerView;
 
@@ -23,6 +30,16 @@ public class NearLauncherFragment extends Fragment{
             , @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_near_launcher, container, false);
         mRecyclerView = view.findViewById(R.id.fragment_near_launcher_recycler_view);
+        setupAdapter();
         return view;
+    }
+
+    private void setupAdapter() {
+        // 打印设备中可启动的应用个数
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(intent, 0);
+        Log.i(TAG, "Found " + activities.size() + " activities.");
     }
 }
